a6692022703e0f537b907403855d5af23c429c37
Faza Ramadhani <53871962+fazapoint@users.noreply.github.com>
2023-10-27 16:06:17 +0700

    answered 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, total) {
+    let result = ''
+    while(total < 0){
+        return 'ERROR';
+    }
+    
+    for (let i = 0; i < total; i++){
+        result += word; 
+    }
+    return result;
 };
 
 // Do not edit below this line
