9d30ce2f30efd4303fe28203eae027ac54fd3050
Faza Ramadhani <53871962+fazapoint@users.noreply.github.com>
2023-10-27 16:23:33 +0700

    answered 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    const reversed = [];
+    for (const letter of word){
+        reversed.unshift(letter)
+    }
 
+    let result = reversed.join('');
+   
+    return result;
 };
 
 // Do not edit below this line
