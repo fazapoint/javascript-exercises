c3eef1568a5cddff089f4943e4e9bc05262bbae1
Fazarama <53871962+fazapoint@users.noreply.github.com>
2023-10-29 07:20:03 +0700

    answered 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    let result = 0;
+    let start = first;
+    let end = second;
 
+    for (let i=0; i < arguments.length; i++){
+        if (arguments[i] < 0 || typeof arguments[i] !== "number"){
+            return 'ERROR';
+        }
+    }
+
+    if (first > second){
+        start = second;
+        end = first;
+    }
+    
+    for (let i=start; i<=end; i++){
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
