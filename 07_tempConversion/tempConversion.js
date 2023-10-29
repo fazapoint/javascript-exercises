ecc4606dba2c24d5340e26fc7846a56dce8fa9d0
Fazarama <53871962+fazapoint@users.noreply.github.com>
2023-10-29 08:34:57 +0700

    answered 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let result = (farenheit - 32) * 5/9;
+  if (result % 1 === 0){
+    return result;
+  }else if (result % 1 !== 0){
+    return Number(result.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let result = celcius * 9/5 + 32;
+  if (result % 1 === 0){
+    return result;
+  }else if (result % 1 !== 0){
+    return Number(result.toFixed(1));
+  }
 };
 
 // Do not edit below this line
