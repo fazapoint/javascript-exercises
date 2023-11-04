1e985302c858e9ea08ae8312cf97e7c96e2ab5f1
Faza Ramadhani <53871962+fazapoint@users.noreply.github.com>
2023-11-04 12:30:57 +0700

    answered 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(x, y) {
+	const result = x + y;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	const result = x - y;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+	const result = x.reduce((sumArr, value) => {
+    return sumArr + value;
+  }, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(x) {
+  const result = x.reduce((multiplyArr, value) => {
+    return multiplyArr * value;
+  }, 1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	const result = x**y;
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x === 0){
+    return 1;
+  }
+  return x * factorial(x - 1);
 };
 
 // Do not edit below this line
