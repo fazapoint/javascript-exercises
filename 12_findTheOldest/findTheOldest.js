f02417805fb1a31f681939e57b9087455d7b2bbb
Faza Ramadhani <53871962+fazapoint@users.noreply.github.com>
2023-11-04 13:54:55 +0700

    answered 12_findTheOldest and edit 11_getTheTitles

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const result = people.sort((person1, person2) => {
+        let age1 = (person1.yearOfDeath || new Date().getFullYear()) - person1.yearOfBirth;
+        let age2 = (person2.yearOfDeath || new Date().getFullYear()) - person2.yearOfBirth;
+        if (age1 > age2){
+            return -1;
+        } else if(age1 < age2){
+            return 1;
+        }
+        return 0;
+    });
+    return result[0];
 };
 
 // Do not edit below this line
